71f63b39dedc8d1e8561326c5e9b18fed2f9c341
bijan.veyssi-galmiche <bijan.veyssi-galmiche@epita.fr>
2022-10-26 17:27:06 +0200

    feat(git): upgrade configuration

--- programs/git.nix
+++ programs/git.nix
@@ -1,7 +1,19 @@
+{ pkgs, ...}:
+
 {
   programs.git = {
     enable = true;
     userEmail = "bijan.veyssi-galmiche@epita.fr";
     userName = "bijan.veyssi-galmiche";
+    signing = {
+        signByDefault = false;
+        key = "055C32062C20D8AD471CC9F9905BB3EB3013C88F";
+    };
+    extraConfig = {
+        pull = {
+            ff = "only";
+            rebase = true;
+        };
+    };
   };
 }
