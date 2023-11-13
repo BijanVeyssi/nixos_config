41ad29dea7b34de642b50db86b18af010dcfe824
bijan.veyssi-galmiche <bijan.veyssi-galmiche@munic.io>
2023-11-13 08:47:41 +0100

    fix(sxhkd): change binding to use jkl; instead of hjkl

--- programs/sxhkd.nix
+++ programs/sxhkd.nix
@@ -13,7 +13,7 @@
         "bspc node -t {tiled,pseudo_tiled,floating,fullscreen}";
       "super + ctrl + {m,x,y,z}" = # Node flags
         "bspc node -g {marked,locked,sticky,private}";
-      "super + {_,shift + }{h,j,k,l}" = # Focus/Swap node(s)
+      "super + {_,shift + }{j,k,l,semicolon}" = # Focus/Swap node(s)
         "bspc node -{f,s} {west,south,north,east}";
       "super + {_,shift + }c" = # Focus next/previous node
         "bspc node -f {next,prev}.local.!hidden.window";
@@ -21,15 +21,15 @@
         "bspc {node,desktop} -f last";
       "super + {_,shift + }{1,2,3,4,5,6,7,8,9,0}" = # Focus/Send to desktop
         "bspc {desktop -f,node -d} '{1,2,3,4,5,6,7,8,9,10}'";
-      "super + ctrl + {h,j,k,l}" = # Preselect direction
+      "super + ctrl + {j,k,l,semicolon}" = # Preselect direction
         "bspc node -p {west,south,north,east}";
       "super + ctrl + {1-9}" = # Preselect ratio
         "bspc node -o 0.{1-9}";
       "super + ctrl + space" = # Cancel preselection
         "bspc node -p cancel";
-      "super + alt + {h,j,k,l}" = # Resize outward
+      "super + alt + {j,k,l,semicolon}" = # Resize outward
         "bspc node -z {left -20 0,bottom 0 20,top 0 -20,right 20 0}";
-      "super + alt + shift + {h,j,k,l}" = # Resize inward
+      "super + alt + shift + {j,k,l,semicolon}" = # Resize inward
         "bspc node -z {right -20 0,top 0 20,bottom 0 -20,left 20 0}";
       "super + {Left,Down,Up,Right}" = # Move floating node
         "bspc node -v {-20 0,0 20,0 -20,20 0}";
