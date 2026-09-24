9782996deb8d3df8dcfd35518a77d6b915cbbb27
bijan.veyssi-galmiche <bijan.veyssi-galmiche@munic.io>
2026-09-24 13:38:59 +0200

    feat(bij-bertha): add virtual mic

--- nixos/common/optional/pipewire.nix
+++ nixos/common/optional/pipewire.nix
@@ -7,7 +7,23 @@
       enable = true;
       support32Bit = true;
     };
-    jack.enable = true;
+
+    extraConfig.client."91-loopback-microphone" = {
+      "context.modules" = [
+        {
+          name = "libpipewire-module-loopback";
+          args = {
+            "capture.props" = {
+              "audio.position" = "MONO"; # Set to MONO for a single channel
+            };
+            "playback.props" = {
+              "media.class" = "Audio/Source"; # Important for the virtual microphone
+              "node.description" = "Virtual Microphone"; # Description for identification
+            };
+          };
+        }
+      ];
+    };
   };
 
   hardware.alsa.enablePersistence = true;
